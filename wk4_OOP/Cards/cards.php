6cb094141e2178987ce7f329165e53dfa8e6683d
StrayTraveler <1792568382@qq.com>
2022-02-19 02:27:33 +0000

    complete the basic task

--- wk4_OOP/Cards/cards.php
+++ wk4_OOP/Cards/cards.php
@@ -30,7 +30,7 @@ class Cards{
     // toString
     public function __toString(){
         // to tell PHP the way to desplay this object when print it or echo it
-        return "{$this->getRank()} of {$this->getSuit()}";
+        return "{$this->getRank()} of {$this->getSuit()}\n";
     }
 
     // judge
@@ -60,27 +60,109 @@ class Cards{
     }
 }
 
-$card1 = new Cards(12, 3);
-$card2 = new Cards(12, 0);
+// test 1
+// $test = new Cards();
+// echo $test;
 
-print $card1."\n";
-print $card2."\n";
+// test2
+// $card1 = new Cards(12, 3);
+// $card2 = new Cards(12, 0);
 
-if (Cards::eq($card1, $card2)){
-    echo "$card1 is equal rank to $card2\n";
-}
-else{
-    echo"$card1 is not equal rank to $card2\n";
-    if (Cards::gt($card1, $card2)){
-        echo "$card1 is greater rank than $card2\n";
+// print $card1."\n";
+// print $card2."\n";
+
+// if (Cards::eq($card1, $card2)){
+//     echo "$card1 is equal rank to $card2\n";
+// }
+// else{
+//     echo"$card1 is not equal rank to $card2\n";
+//     if (Cards::gt($card1, $card2)){
+//         echo "$card1 is greater rank than $card2\n";
+//     }
+//     if (Cards::lt($card1, $card2)){
+//         echo "$card1 is less rank than $card2\n";
+//     }
+// }
+
+
+class Deck{
+
+    protected $cards = [];
+
+    // create 52 cards
+    public function __construct(){
+        for($suit = 0; $suit < 4; $suit++){
+            for($rank = 0; $rank < 13; $rank++){
+                $this->cards[] = new Cards($rank, $suit);
+            }
+        }
     }
-    if (Cards::lt($card1, $card2)){
-        echo "$card1 is less rank than $card2\n";
+
+    // deal cards from deck
+    public function deal(){
+        return array_pop($this->cards);
+    }
+
+    // judge if the deck is empty
+    public function isEmpty(){
+        return count($this->cards) == 0;
+    }
+
+    // shuffle the cards in the deck
+    public function shuffle(){
+        return shuffle($this->cards);
+    }
+
+    // print the deck
+    public function __toString(){
+        $output = "";
+        foreach($this->cards as $card){
+            $output .= $card ."\n";
+        }
+        return $output;
+    }
+}
+
+// test 3
+// $deck1 = new Deck();
+// print $deck1;
+// $deck1->shuffle();
+// print $deck1;
+// $card1 = $deck1->deal();
+// $card2 = $deck1->deal();
+
+// print $card1."\n";
+// print $card2;
+
+
+// The cards held by players
+class Hand extends Deck{
+    public $label;
+
+    // construct a hand of cards
+    public function __construct($label = ''){
+        $this->label = $label;
+        $this->cards = [];
+    }
+    // add a card to hand
+    public function addCard(Cards $card){
+        $this->cards[] = $card;
     }
 }
 
 
-// testing 1
-$test = new Cards();
-echo $test;
+// test 4
+// $deck = new Deck();
+// $deck->shuffle();
+// $hand = new Hand("Joe");
+// print "Dealing {$hand->label}'s cards\n";
+// for ($x = 0; $x < 3; $x++){
+//     $hand->addCard($deck->deal());
+// }
+
+// print "{$hand->label}'s hand\n";
+// print "{$hand->deal()}";
+// print "{$hand->deal()}";
+// print "{$hand->deal()}";
+
 ?>
